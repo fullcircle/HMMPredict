98f10f12c757fc4c79827d426f7ddba4f5a4f9f7
Kevin McC <kevin@fullcirclenetworks.com>
2024-04-18 01:49:01 +0000

    Initial commit

---
+++ hmmpredict.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+import yfinance as yf
+from hmmlearn import hmm
+import matplotlib.pyplot as plt
+
+
+
+data = yf.download("NVDA")
+returns = np.log(data.Close / data.Close.shift(1))
+range = (data.High - data.Low)
+features = pd.concat([returns, range], axis=1).dropna()
+features.columns = ["returns", "range"]
+
+model = hmm.GaussianHMM(
+  n_components=3,
+  covariance_type="full",
+  n_iter=1000,
+)
+model.fit(features)
+states = pd.Series(model.predict(features), index=data.index[1:])
+states.name = "state"
+states.hist()
+
+color_map = {
+    0.0: "green",
+    1.0: "orange",
+    2.0: "red"
+}
+(
+    pd.concat([data.Close, states], axis=1)
+    .dropna()
+    .set_index("state", append=True)
+    .Close
+    .unstack("state")
+    .plot(color=color_map, figsize=[16, 12])
+)
+# Show the plot
+plt.show()
